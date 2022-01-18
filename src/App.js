f1f5f6c590e8a82045fde43b7d9da5ab48769c17
Mark <duffy_mark@hotmail.com>
2022-01-18 10:29:37 -0400

    added background, more functionality

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
-import { useEffect, useState } from 'react';
 import axios from 'axios';
+import './App.css';
+import { useEffect, useState } from 'react';
 import Header from './Header.js';
 import Footer from './Footer.js'
-import './App.css';
+
 
 function App() {
 
   //useStates
 
+const apiKey = 'b708779c14f64973950cdad688a8ee24';
+const baseUrl= 'https://api.weatherbit.io/v2.0/current';
+
+const [weather, setWeather] = useState([]);
 const [userInput, setUserInput] = useState('');
 const [searchTerm, setSearchTerm] = useState('');
-const [weather, setWeather] = useState([]);
+
 
 // call API to BitWeather
 
-useEffect(() => {
-    const apiKey = 'b708779c14f64973950cdad688a8ee24';
+
+const handleClick = async() => {
+//useEffect(() => {
     axios({
-      url: 'http://api.weatherbit.io/v2.0/current',
       method: 'GET',
+      url: baseUrl,
       dataResponse: 'json',
       params: {
         key: apiKey,
+        // country_code: 'CA',
         country: 'Canada',
-        city: searchTerm
+        city: userInput
       }
     }).then( (response) => {
       console.log(response.data.data);
+      //console.log(response.data.data.city_id)
       setWeather(response.data.data); //update "weather" state with response from API
-      console.log(setWeather)
+      //console.log(setWeather)
     });
-  }, [searchTerm]) // }, [searchTerm]) add empty array to prevent callback func from running during every component re-render
+  //}, [searchTerm]) // }, [searchTerm]) add empty array to prevent callback func from running during every component re-render
+}
 
 const handleInput = (event) => {
 console.log('is this working?', event.target.value);
@@ -40,25 +49,30 @@ setUserInput(event.target.value);
 const handleSubmit = (event) => {
 event.preventDefault();
 setSearchTerm(userInput);
+setUserInput('');
 }
 
   return (
     <div className="App">
+
       <Header />
 
       <form onSubmit={ handleSubmit }>
         <label htmlFor="search">What City Would You Like to Search For:</label>
         <input type="text" id="search" onChange={ handleInput } value={userInput} />
-        <button>Is It Sweater Weather?</button>
+        <button onClick = {handleClick} >Is It Sweater Weather?</button>
       </form>
 
       {weather.map((todaysWeather) => {
         return (
-          
-        <>
+        <div key={todaysWeather.city_name}>
           <p> { todaysWeather.city_name}  </p>
           <p> { todaysWeather.temp } </p>
-        </> 
+          <p> { todaysWeather.weather.description } </p>
+          
+      
+      <img src= {`https://www.weatherbit.io/static/img/icons/${todaysWeather.weather.icon}.png`} alt='Icon of the weather description' />
+        </div> 
           // add a ternary operator if weather is sweater weather or not
           // (https://github.com/HackerYou/bootcamp-notes/blob/main/react-and-firebase/intro-to-react.md)
         );
@@ -71,5 +85,7 @@ setSearchTerm(userInput);
 
 
 export default App;
+
+// key: city_id
 
 
--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,12 @@
 const Footer = () => {
   return (
     <footer>
-        <h2>Created for Juno College</h2>
-        <h3>Created by Mark Duffy</h3>
+      <div className="wrapper">
+        <div className="footerLinks">
+            <a href="https://junocollege.com/" target="blank" aria-label="Go to Juno College's website">Created at Juno College</a>
+            <p>Created by <a href="https://github.com/duffymark" target="blank" aria-label="Go to Mark Duffy's Github">Mark Duffy</a></p>
+          </div>
+        </div>
     </footer>
   )
 }
